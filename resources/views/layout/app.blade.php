66a92153f9fcde61231e33fedef2ffc1264e388e
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2026-06-28 16:23:03 +0300

    rtl dashboard

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    Soft UI Dashboard by Creative Tim
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- CSS Files -->
+  @vite([
+    'resources/assets/css/nucleo-icons.css',
+    'resources/assets/css/nucleo-svg.css',
+    'resources/assets/css/soft-ui-dashboard.css',
+   ])
+</head>
+
+<body class="g-sidenav-show rtl bg-gray-100">
+    @include('components.aside')
+    <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
+        @include('components.nav')
+        @yield('content')
+    </main>
+
+    @include('components.scripts')
+
+</body>
+
+</html>
+
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/assets/css/nucleo-icons.css',
+                'resources/assets/css/nucleo-svg.css',
+                'resources/assets/css/soft-ui-dashboard.css',
+                'resources/assets/js/core/popper.min.js',
+                'resources/assets/js/core/bootstrap.min.js',
+                'resources/assets/js/plugins/perfect-scrollbar.min.js',
+                'resources/assets/js/plugins/smooth-scrollbar.min.js',
+                'resources/assets/js/plugins/chartjs.min.js',
+                'resources/assets/js/soft-ui-dashboard.min.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
